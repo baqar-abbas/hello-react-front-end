dae428e4c789cbd9ab2d1816d5bed2abf3bf3ec7
baqar-abbas <baqar007@hotmail.com>
2023-09-24 06:26:10 +0500

    Removed Linter errors

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import greetingReducer from './greeting/greetingSlice';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     greeting: greetingReducer,
   },
